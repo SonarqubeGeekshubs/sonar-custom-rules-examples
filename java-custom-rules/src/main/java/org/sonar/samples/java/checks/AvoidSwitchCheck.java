6847dc8389882bec0fd5e34ab790059cc41c76e2
Ricardo García Fernández <ricardogarfe@gmail.com>
2017-03-08 23:43:14 +0100

    Add test and rule to avoid switch

---
+++ java-custom-rules/src/main/java/org/sonar/samples/java/checks/AvoidSwitchCheck.java
@@ -0,0 +1,24 @@
+package org.sonar.samples.java.checks;
+
+import com.google.common.collect.ImmutableList;
+import org.sonar.check.Rule;
+import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.tree.SwitchStatementTree;
+import org.sonar.plugins.java.api.tree.Tree;
+
+import java.util.List;
+
+@Rule(key = "AvoidSwitch")
+public class AvoidSwitchCheck extends IssuableSubscriptionVisitor {
+
+  @Override
+  public List<Tree.Kind> nodesToVisit() {
+    return ImmutableList.of(Tree.Kind.SWITCH_STATEMENT);
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    SwitchStatementTree switchStatementTree = (SwitchStatementTree) tree;
+    reportIssue(switchStatementTree, "Remove this \"switch\" statement and develop with Polymorphism.");
+  }
+}
