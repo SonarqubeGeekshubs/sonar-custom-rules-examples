8d7c037add0059e3b4f84f8edfb4ac0dae98945c
Ricardo García Fernández <ricardogarfe@gmail.com>
2017-03-14 23:14:11 +0100

    Test Class for Avoid Masked Switch Rule

---
+++ java-custom-rules/src/test/java/org/sonar/samples/java/checks/AvoidMaskedSwitchCheckTest.java
@@ -0,0 +1,21 @@
+package org.sonar.samples.java.checks;
+
+import org.junit.Test;
+import org.sonar.java.checks.verifier.JavaCheckVerifier;
+
+public class AvoidMaskedSwitchCheckTest {
+
+    @Test
+    public void detected() {
+
+        // Use an instance of the check under test to raise the issue.
+        AvoidMaskedSwitchCheck check = new AvoidMaskedSwitchCheck();
+
+        // Verifies that the check will raise the adequate issues with the expected
+        // message.
+        // In the test file, lines which should raise an issue have been commented
+        // out
+        // by using the following syntax: "// Noncompliant {{EXPECTED_MESSAGE}}"
+        JavaCheckVerifier.verify("src/test/files/AvoidMaskedSwitchCheck.java", check);
+    }
+}
